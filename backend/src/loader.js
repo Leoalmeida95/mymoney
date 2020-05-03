0b65e559ef98979ef2ff801762855928a90dddc0
leoalmeida95 <leo.almeida.silva@hotmail.com>
2020-05-03 12:12:33 -0300

    billingCycle services e routers

--- backend/src/loader.js
+++ backend/src/loader.js
@@ -1,2 +1,4 @@
-require('./config/server')
+const server = require('./config/server')
 require('./config/database')
+//passa o server de parametro para configuração das rotas
+require('./config/routes')(server)
