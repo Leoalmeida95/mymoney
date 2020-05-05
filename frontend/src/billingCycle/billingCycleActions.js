f216064179426b95b22c47f53f1de151815bb46f
leoalmeida95 <leo.almeida.silva@hotmail.com>
2020-05-05 17:33:23 -0300

    dispatch posting billingCycle

--- frontend/src/billingCycle/billingCycleActions.js
+++ frontend/src/billingCycle/billingCycleActions.js
@@ -1,5 +1,7 @@
 import axios from 'axios'
 import {toastr} from 'react-redux-toastr'
+import {reset as resetForm } from 'redux-form'
+import {showTabs, selectTab} from '../common/tab/tabActions'
 
 import URL from '../config/server'
 
@@ -15,15 +17,21 @@ export function getList(){
 }
 
 export function create(values){
-    const request = axios.post(`${URL}/billingCycles`, values)
-                        .then(resp => {
-                             toastr.success('Sucesso','Operação realizada com sucesso.')
+    return dispatch => {
+        axios.post(`${URL}/billingCycles`, values)
+                            .then(resp => {
+                                 toastr.success('Sucesso','Operação realizada com sucesso.')
+                                 //só é possível passar o array pro dispatch graças ao redux multi
+                                 dispatch([
+                                    //todos os métodos são actions
+                                    resetForm('billingCycleForm'),//passa de parametro o 'id' definido na criação do Form
+                                    getList(),
+                                    selectTab('tabList'),
+                                    showTabs('tabList','tabCreate')
+                                 ])
+                                })
+                            .catch(err => {
+                                err.response.data.errors.forEach(e => toastr.error('Erro', e))
                             })
-                        .catch(err => {
-                            err.response.data.errors.forEach(e => toastr.error('Erro', e))
-                        })
-    return{
-        type: 'TEMP',
-        payload: request
     }
 }
